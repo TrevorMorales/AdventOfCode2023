d5f7a8689e65c45f41c3ae317909b68e16a78dad
TrevorMorales <trevorjmorales@gmail.com>
2023-12-01 13:56:18 -0500

    Consolidated loops

--- Trebuchet.cpp
+++ Trebuchet.cpp
@@ -11,25 +11,24 @@ int ParseLine(string ToParse)
 {
 	bool FirstFound = false;
 	int First = ' ', Last = ' ';
-	for (int c = 0; c < ToParse.length(); c++)
-	{
-		if (ToParse[c] >= 65 && ToParse[c] <= 90)
-		{
-			ToParse[c] += 32;
-		}
-	}
+
 	for (int i = 0; i < ToParse.length(); i++)
 	{
-		if (ToParse[i] >= 48 && ToParse[i] <= 57)
+		char CheckChar = ToParse[i];
+		if (CheckChar >= 65 && CheckChar <= 90)
+		{
+			ToParse[i] += 32;
+		}
+		if (CheckChar >= 48 && CheckChar <= 57)
 		{
 			if (!FirstFound)
 			{
-				First = ToParse[i] - 48;
+				First = CheckChar - 48;
 				FirstFound = true;
 			}
 			else
 			{
-				Last = ToParse[i] - 48;
+				Last = CheckChar - 48;
 			}
 		}
 		else
